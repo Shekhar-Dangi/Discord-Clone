13cc03443252624eaf8061a6d5e1c94378cf3bb9
Shekhar Dangi <arpitt682@gmail.com>
2023-09-03 22:38:28 +0530

    server url

--- client/src/axios-config.js
+++ client/src/axios-config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenFromCookies } from "./cookieUtil";
 
 const reqInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://discord-clone-omega-livid.vercel.app/",
 });
 
 reqInstance.interceptors.request.use(
